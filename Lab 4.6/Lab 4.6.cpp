184e93051066bc262cde4c0c81447f928a5ecfc0
s1lencebtw <Vitalii.Letnianchyn.RI.2023@lpnu.ua>
2023-10-15 17:05:44 +0300

    Add project files.

---
+++ Lab 4.6/Lab 4.6.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n, k;
+	double S, P;
+
+	P = 1;
+	n = 1;
+	while (n <= 20) {
+		S = 0;
+		k = n;
+		while (k <= 20) {
+			S += k;
+			k++;
+		}
+		P *= (pow(n, 2) + pow(S, 2)) / (n + 1);
+		n++;
+	}
+	cout << "P: " << P << endl;
+	P = 1;
+	n = 1;
+	do {
+		S = 0;
+		k = n;
+		do {
+			S += k;
+			k++;
+		} while (k <= 20);
+		P *= (pow(n, 2) + pow(S, 2)) / (n + 1);
+		n++;
+	} while (n <= 20);
+	cout << "P: " << P << endl;
+
+	P = 1;
+	for (n = 1; n <= 20; n++) {
+		S = 0;
+		for (k = n; k <= 20; k++) {
+			S += k;
+		}
+		P *= (pow(n, 2) + pow(S, 2)) / (n + 1);
+	}
+	cout << "P: " << P << endl;
+
+	P = 1;
+	for (n = 20; n >= 1; n--) {
+		S = 0;
+		for (k = 20; k >= n; k--) {
+			S += k;
+		}
+		P *= (pow(n, 2) + pow(S, 2)) / (n + 1);
+	}
+	cout << "P: " << P << endl;
+	return 0;
+}
